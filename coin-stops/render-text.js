7f4e8c7bb509b6f76f9967c9f2c84518374a40a0
Derek Jamison <jamisonderek@users.noreply.github.com>
2021-01-22 17:21:39 -0800

    Create render-text.js

---
+++ coin-stops/render-text.js
@@ -0,0 +1,33 @@
+pm.visualizer.set(
+    `
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+        {{#each response}}
+            <h2>{{stopName}}</h2>
+            {{mode}}|{{headsign}}|{{transportName}}
+
+            <table bgcolor="#FFFFFF" border="2">
+            {{#each nearby}}
+            <tr>
+                <td>{{name}}</td>
+                <td>{{stars}} stars of 5</td>
+                <td>{{review_count}} Yelp reviews</td>
+                <td>{{coins}} Yelp dollar signs</td>
+                <td>{{categories}}</td>
+                <td>{{transactions}}</td>
+                <td><img src={{image}} width="100" height="100" display="inline"></img></td>
+                <td>{{address}}</td>
+                <td>{{lat}},{{long}}</td>
+                <td>{{distance}} meters</td>
+                <td>{{link}}</td>
+            </tr>
+            {{/each}}
+            </table>
+
+        {{/each}}
+    `
+    ,
+    {
+        response: aggregateResults
+    }
+);

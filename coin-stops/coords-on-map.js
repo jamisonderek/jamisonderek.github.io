012714b0da28e0e13b86b78b9b7a3fb29697d14c
Derek Jamison <jamisonderek@users.noreply.github.com>
2021-01-22 19:05:41 -0800

    Update coords-on-map.js

--- coin-stops/coords-on-map.js
+++ coin-stops/coords-on-map.js
@@ -2,7 +2,7 @@ console.log('This is the coords-on-map.js file.');
 
 const icons = [
     'https://dummyimage.com/40x40/ccc/ccc',
-    'https://dummyimage.com/40x40/6fd/6fd',
+    'https://dummyimage.com/40x40/0f0/0f0',
     'https://dummyimage.com/40x40/ff0/ff0',
     'https://dummyimage.com/40x40/fa0/fa0',
     'https://dummyimage.com/40x40/f00/f00'
